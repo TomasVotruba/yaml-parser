4ad8f2febcbe6148effb91f953a79dc975caefcb
Tomas Votruba <tomas.vot@gmail.com>
2022-05-27 21:34:34 +0200

    init

---
+++ src/YAMLParser.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+final class YAMLParser
+{
+    public function run()
+    {
+    }
+}
+
+$yamlParser = new YAMLParser();
+$yamlParser->run();

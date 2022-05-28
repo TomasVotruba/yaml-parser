0844ee10913f27f4fab47225518de4e95074b14a
Tomas Votruba <tomas.vot@gmail.com>
2022-05-29 01:08:15 +0200

    make use of fixtures

--- tests/NodeVisitor/YAMLNodeVisitorTest.php
+++ tests/NodeVisitor/YAMLNodeVisitorTest.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace YAMLParser\Tests\NodeVisitor;
 
 use PHPUnit\Framework\TestCase;
+use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
+use Symplify\EasyTesting\StaticFixtureSplitter;
+use Symplify\SmartFileSystem\SmartFileInfo;
 use YAMLParser\NodeTraverser\NodeTraverser;
 use YAMLParser\NodeVisitor\Symfony\SubscriberToListenerNodeVisitor;
 use YAMLParser\Parser\NEONParser;
@@ -18,18 +21,27 @@ final class YAMLNodeVisitorTest extends TestCase
         $this->neonParser = new NEONParser();
     }
 
-    public function test(): void
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fileInfo): void
     {
+        $inputAndExpected = StaticFixtureSplitter::splitFileInfoToInputAndExpected($fileInfo);
 
         // make the change happen
         $nodeTraverser = new NodeTraverser([
             new SubscriberToListenerNodeVisitor()
         ]);
 
-        $node = $this->neonParser->parseFile(__DIR__ . '/Fixture/before_services_listener.yaml');
+        $node = $this->neonParser->parse($inputAndExpected->getInput());
         $nodeTraverser->traverse($node);
 
         $printedNode = rtrim($node->toString()) . PHP_EOL;
-        $this->assertStringEqualsFile(__DIR__ . '/Fixture/after_services_subscriber.yaml', $printedNode);
+        $this->assertEquals($inputAndExpected->getExpected(), $printedNode);
+    }
+
+    public function provideData()
+    {
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixture', '*.yaml');
     }
 }

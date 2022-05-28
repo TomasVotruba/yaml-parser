5938d39851db4c377e4431d02c874da0e8510c69
Tomas Votruba <tomas.vot@gmail.com>
2022-05-28 15:57:26 +0200

    add ci tests

---
+++ src/Contract/NodeVisitor/NodeVisitorInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YAMLParser\Contract\NodeVisitor;
+
+use Nette\Neon\Node;
+
+/**
+ * @template TNode as \Nette\Neon\Node
+ */
+interface NodeVisitorInterface
+{
+    /**
+     * @return class-string<TNode>
+     */
+    public function getNodeType(): string;
+
+    /**
+     * @param TNode $node
+     */
+    public function refactor(Node $node);
+}
---
+++ src/NodeVisitor/Symfony/SubscriberToListenerNodeVisitor.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace YAMLParser\NodeVisitor\Symfony;
+
+final class SubscriberToListenerNodeVisitor
+{
+
+}
